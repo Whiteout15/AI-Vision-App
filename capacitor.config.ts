fae1294e4fb4e601b0a47d9da9e525109d9e62b0
Cristian <cristianbolanos@yahoo.com>
2023-12-11 00:55:52 -0500

    Updates

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Ai-React',
+  appName: 'Vision AI',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
